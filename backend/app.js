20f14ac0a988426d590911a3082f21526b821482
Siddhi Vinayaka <crazythoughts1729@gmail.com>
2025-11-19 13:29:33 +0530

    Fix catch-all route for Express 5

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ const frontendDistPath = path.join(__dirname, "../frontend/dist");
 app.use(express.static(frontendDistPath));
 
 // Catch-all route to serve index.html for client-side routing
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
